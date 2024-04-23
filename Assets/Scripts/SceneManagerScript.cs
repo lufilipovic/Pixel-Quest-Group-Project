aa912d98e53909a67270dcc9f54cab5419945b9f
Lucija Filipovic <lucija_filipovic_@hotmail.com>
2024-04-23 19:43:36 +0300

    adding art for menu, start screen, and menu around all scenes

--- Assets/Scripts/SceneManagerScript.cs
+++ Assets/Scripts/SceneManagerScript.cs
@@ -5,15 +5,64 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagerScript : MonoBehaviour
 {
-   
+
+    public GameObject howToPlayPanel;
+
     private void Start()
     {
-
+        howToPlayPanel.SetActive(false);
+        Time.timeScale = 1f;
     }
 
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (IsGamePaused())
+            {
+                ResumeGame();
+                print("Resuming game: " + Time.timeScale);
+            }
+            else
+            {
+                PauseGame();
+                print("Pausing game: " + Time.timeScale);
+            }
+        }
+    }
+
+    public void PlayGame()
+    {
+        //Load next scene in the build level
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        print("Game Exited");
+
+        //Quit the application
+        Application.Quit();
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        howToPlayPanel.SetActive(true);
+        print("Pausing game: " + Time.timeScale);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        howToPlayPanel.SetActive(false);
+        print("Resuming game: " + Time.timeScale);
+    }
+
+    // Function to check if the game is currently paused
+    private bool IsGamePaused()
+    {
+        return Time.timeScale == 0f;
     }
 
     public void LoadScene(string sceneName)
@@ -21,5 +70,6 @@ public class SceneManagerScript : MonoBehaviour
         SceneManager.LoadScene(sceneName);
 
     }
+
 
 }
