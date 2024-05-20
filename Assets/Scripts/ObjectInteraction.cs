923558c4d702e3140698f14043f96daff07f8bc4
Lucija Filipovic <lucija_filipovic_@hotmail.com>
2024-05-20 03:27:35 +0300

    Whole puzzle room finished
    Implemented the puzzle in the puzzle room, implemented panels for objects, and piano, added dialogue from the pet for hints, added checking of the sequence in the puzzle, showing the key once the puzzle is solved, etc..

--- Assets/Scripts/ObjectInteraction.cs
+++ Assets/Scripts/ObjectInteraction.cs
@@ -5,35 +5,42 @@ using UnityEngine.UI;
 
 public class ObjectInteraction : MonoBehaviour
 {
-
     private GameObject player;
     public float interactionRange = 2f;
-    public GameObject interactableObject; 
-             
+    public GameObject interactableObject;
+    public GameObject interactionPanel; // Reference to the UI Panel
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        interactionPanel.SetActive(false); // Ensure the panel is initially inactive
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            // Check if the player is in range to pick up the object
-            if (IsPlayerInRange())
+            if (interactionPanel.activeSelf)
+            {
+                interactionPanel.SetActive(false); // Close the interaction panel if it's already open
+            }
+            else if (IsPlayerInRange())
             {
                 print("Hello I am " + interactableObject.name);
+                interactionPanel.SetActive(true); // Show the interaction panel if it's not open and the player is in range
             }
         }
     }
 
-
     private bool IsPlayerInRange()
     {
         // Calculate the distance between the player and the object
-        float distance = Vector2.Distance(transform.position, player.transform.position);
+        float distance = Vector3.Distance(transform.position, player.transform.position);
         return distance <= interactionRange;
     }
 
+    public void ClosePanel()
+    {
+        interactionPanel.SetActive(false); // Hide the interaction panel
+    }
 }
